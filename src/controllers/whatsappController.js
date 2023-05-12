4183566066b75769900ef9d2cd0c7c271f1d0960
dlwlrmla <joakomask@gmail.com>
2023-05-11 21:32:13 -0400

    la wea 1

--- src/controllers/whatsappController.js
+++ src/controllers/whatsappController.js
@@ -1,4 +1,24 @@
+import dotenv from  "dotenv"
+dotenv.config()
+
 export const verifyToken = (req, res) => {
+
+    try {
+        var accesToken=process.env.ACCESSTOKEN
+        var token = req.query["hub.verify_token"]
+        var challenge = req.query["hub.challenge"]
+
+        if(challenge!= null && token!= null && token == accesToken){
+            res.send(challenge)
+        }else{
+            res.status(400).send()
+        }
+
+    } catch (error) {
+        res.status(400).send()
+    }
+
+
     res.send("verify Token")
 }
 
