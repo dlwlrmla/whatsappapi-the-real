9a913feadfe019cd4ac09bf5209bfb878824aaa0
dlwlrmla <joakomask@gmail.com>
2023-05-11 21:21:36 -0400

    a

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import express from "express"
 import router from "./routes/routes.js"
 import dotenv from  "dotenv"
 dotenv.config()
+const PORT = 3000||process.env.PORT
 const app = express()
 app.use(express.json())
 app.use('/whatsapp', router)
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log(`running on http://localhost:3000/`)
     console.log(process.env.HOLA)
 })
